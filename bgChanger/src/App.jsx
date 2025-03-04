9cb4801ffe7566163a90c8bb5a51dd79ce3d8b09
Smit Tank <smittank02@gmail.com>
2025-03-04 13:38:25 +0530

    effect

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [color, setcolor] = useState("lightseagreen")
 
   return (
-    <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+    <div className="w-full h-screen duration-500" style={{backgroundColor:color}}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-3 py-2 ">
 
       <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl">
@@ -15,7 +15,7 @@ function App() {
       style={{backgroundColor:"red"}}>Red</button>
 
       <button   onClick={()=>setcolor("green")} 
-       className="outline-none px-4 py-1 rounded-full text-white shadow-lg text-2xl "
+       className="outline-none px-4 py-1 rounded-full text-white shadow-lg text-2xl"
       style={{backgroundColor:"green"}}>Green</button>
 
       <button   onClick={()=>setcolor("blue")} 
