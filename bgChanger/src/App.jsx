c1fcc53fed2d311eb54c7b34c7e092689d4c8d9a
Smit Tank <smittank02@gmail.com>
2025-03-04 13:49:46 +0530

    Updated App.css and App.jsx

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [color, setcolor] = useState("lightseagreen")
 
   return (
-    <div className="w-full h-screen duration-500" style={{backgroundColor:color}}>
+    <div className="w-full h-screen duration-1000 ease-in-out" style={{backgroundColor:color}}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-3 py-2 ">
 
       <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl">
